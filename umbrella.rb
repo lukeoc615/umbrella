76a0fe1aff854c66556183f084bcc765b5e0a620
Luke O'Connell <147653898+lukeoc615@users.noreply.github.com>
2023-10-31 20:08:20 +0000

    created latitude and longitude variables

--- umbrella.rb
+++ umbrella.rb
@@ -1,3 +1,32 @@
+pp "Where are you located?"
+
+user_location = gets.chomp
+
+pp user_location
+
+maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location +"&key=AIzaSyDKz4Y3bvrTsWpPRNn9ab55OkmcwZxLOHI
+"
+require "http"
+
+resp = HTTP.get(maps_url)
+
+raw_response = resp.to_s
+
+require 'json'
+
+parsed_response = JSON.parse(raw_response)
+
+results = parsed_response.fetch("results")
+
+first_result = results.at(0)
+
+geo = first_result.fetch("geometry")
+
+loc = geo.fetch("location")
+
+pp latitude = loc.fetch("lat")
+pp longitude = loc.fetch("lng")
+
 pirate_weather_api_key = "3RrQrvLmiUayQ84JSxL8D2aXw99yRKlx1N4qFDUE"
 
 # I've already created a string variable above: pirate_weather_api_key
